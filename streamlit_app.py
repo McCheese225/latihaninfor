9f322af28f9121f8291ee6cfb497026b6fadd8cb
McCheese225 <mccheese225@gmail.com>
2025-05-21 19:42:18 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,7 @@
 import streamlit as st
 
-st.title("my epic gaming wen" )
+st.title("my epic tacticool cag gaming web, the most tactical web ever" )
 st.write(
-    "10/10 web –IGN"
+    "10/10 web –IGN i'm thinking miku miku owewu" 
 )
 st.image("aziknpzd2w971.png", width= 1000)
