23ecd94085b35bc5a7c559dac63d2ef6dfe186e3
McCheese225 <mccheese225@gmail.com>
2025-05-21 09:44:36 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -2,6 +2,6 @@ import streamlit as st
 
 st.title("Selamat datang di web informatika cag ayyubi XH" )
 st.write(
-    "ngoding seru bersama akuuu."
+    "ngoding seru bersama akuuu"
 )
 st.image("aziknpzd2w971.png", width= 1000)
