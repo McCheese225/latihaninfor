53462eaeec5bdcd44831b073f0488579be723607
McCheese225 <mccheese225@gmail.com>
2025-05-21 09:30:51 +0700

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -4,4 +4,4 @@ st.title("Selamat datang di web informatika" )
 st.write(
     "ngoding seru bersama bapak hendri."
 )
-st.image("aziknpzd2w971.png", width= 200)
+st.image("aziknpzd2w971.png", width= 500)
